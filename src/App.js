186d28a50cef02199fc39a36e8e259707efd55f1
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-01-06 22:50:07 +0330

    field ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import { Field } from './components/field/field';
 import { Language } from './components/language/language';
 import { Translate } from './components/translate/translate';
 
 function App() {
+  const [text, setText] = useState('');
   return (
     <div>
-      <Field />
+      <Field setText={setText} />
       <Language />
       <hr />
       <Translate />
--- src/components/field/field.jsx
+++ src/components/field/field.jsx
@@ -1,15 +1,14 @@
-
-
-
-
-
-
-
- export const Field = () => {
-
-
-
-
-    return <div></div>
+export const Field = ({ setText }) => {
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          if (e.target.value) setText(e.target.value);
+        }}
+        type="text"
+        id="input"
+      />
+      <label htmlFor="input"> :متن خود را وارد کنید</label>
+    </div>
+  );
 };
-
